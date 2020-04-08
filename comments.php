049061cfb65e4c66cd058ea0e2a55ecb99db4fef
mamunhshimul <mamunhshimul@gmail.com>
2020-04-08 21:28:28 +0600

    added comments.php

---
+++ comments.php
@@ -0,0 +1,27 @@
+ 
+
+
+
+
+                <div id="respond" class="comment-respond">
+                  <h2 class="comment-reply-title">leave your comment</h2>
+                  <div class="comment-form-box">
+                    <form method="post" id="commentform" class="comment-form">
+                      <p class="comment-form-author">
+                        <input id="author" name="author" placeholder="Name :" size="30" maxlength="245" required="required" type="text"/>
+                      </p>
+                      <p class="comment-form-email">
+                        <input id="email" name="email" placeholder="e-mail :" required="required" type="email"/>
+                      </p>
+                      <p class="comment-form-url">
+                        <input id="url" name="url" placeholder="website :" type="url"/>
+                      </p>
+                      <p class="comment-form-comment">
+                        <textarea id="comment" name="comment" placeholder="comment :" rows="5" required="required"></textarea>
+                      </p>
+                      <p class="form-submit">
+                        <input name="submit" class="submit" value="Post Comment" type="submit"/>
+                      </p>
+                    </form>
+                  </div>
+                </div><!-- Comment Form /- -->
