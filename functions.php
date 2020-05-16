77bdacca77269c58f0c78074fc59d1e9ef5c9cf4
mamunhshimul <mamunhshimul@gmail.com>
2020-05-16 21:38:17 +0600

    added widgets in file

--- functions.php
+++ functions.php
@@ -27,69 +27,11 @@ add_action( 'after_setup_theme', 'iBlogger_setup' );
         'footer_menu' => esc_html__( 'Fpoter', 'iBlogger' ),
     ) );
 
+ 
 /**
-sidebar 
---------------*/
-function iBlogger_widgets_init() { 
-
-    register_sidebar( array(
-        'name' =>__( 'Foote text 1', 'iBlogger'),
-        'id' => 'foote-text-1',
-        'description' => __( 'Appears on the static front page template', 'iBlogger' ),
-        'before_widget' => '<aside id="%1$s" class="widget widget_about">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ) ); 
- 
-    register_sidebar( array(
-        'name' =>__( 'Foote text 2', 'iBlogger'),
-        'id' => 'foote-text-2',
-        'description' => __( 'Appears on the static front page template', 'iBlogger' ),
-        'before_widget' => '<aside id="%1$s" class="widget widget_latestposts">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ) ); 
- 
-    register_sidebar( array(
-        'name' =>__( 'Foote text 3', 'iBlogger'),
-        'id' => 'foote-text-3',
-        'description' => __( 'Appears on the static front page template', 'iBlogger' ),
-        'before_widget' => '<aside id="%1$s" class="widget widget_twitter">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ) ); 
- 
-    register_sidebar( array(
-        'name' =>__( 'Foote text 4', 'iBlogger'),
-        'id' => 'foote-text-4',
-        'description' => __( 'Appears on the static front page template', 'iBlogger' ),
-        'before_widget' => '<aside id="%1$s" class="widget widget_flickr">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ) );
-
-     register_sidebar( array(
-        'name' =>__( 'Fooeter bottom', 'iBlogger'),
-        'id' => 'Fooeterbottom',
-        'description' => __( 'Update your footer title', 'iBlogger' ),
-        'before_widget' => '<aside id="%1$s" class="widget widget_flickr">',
-        'after_widget' => '</aside>',
-        'before_title' => '<h3 class="widget-title">',
-        'after_title' => '</h3>',
-    ) );
-   
-
-  
-
-}
- 
-add_action( 'widgets_init', 'iBlogger_widgets_init' );  
-
-
+* Side Bar
+*/
+require_once get_theme_file_path('/template-parts/sidebar/widget-init.php');
 
 /**
  * Load custom WordPress nav walker.
